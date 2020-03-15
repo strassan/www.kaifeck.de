f35206fc12406234440dcf8e842527f1fd005bb0
Andreas Straßer <andistra@online.de>
2020-03-15 20:30:38 +0100

    Make email password work on server

--- php/send_mail.php
+++ php/send_mail.php
@@ -7,7 +7,7 @@ require_once "Mail.php";
 
 $host = "ssl://smtp.zoho.eu";
 $username = "band@kaifeck.de";
-$password = file_get_contents("pwd");
+$password = file_get_contents("/var/www/pwd");
 $port = "465";
 $email_sender = $_POST['email'];
 $email_noreply = "noreply@kaifeck.de";
