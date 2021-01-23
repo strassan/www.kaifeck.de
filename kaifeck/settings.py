f52ba90480199609bd74885c9f27f92ac78f8626
Andreas Strasser <andistra@online.de>
2021-01-23 16:31:14 +0100

    Duplicate SECRET_KEY to settings.py

--- kaifeck/settings.py
+++ kaifeck/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'sg8xoww6brqiw1*du#m3tc)iieo%%i9xhp&@1ppbu_b)*@ds=8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
