3602c0b48670d48c8130cc6fb6aa9001979b2b2f
Andreas Strasser <strassan@kaifeck.de>
2021-05-14 17:09:43 +0200

    Make thumbnails for better performance
    Especially on smartphones the large images led to a laggy scrolling behaviour

--- linkpage/models.py
+++ linkpage/models.py
@@ -1,5 +1,14 @@
+import os
+from io import BytesIO
+
+from django.core.files.base import ContentFile
 from django.db import models
 from django.utils import timezone
+
+from PIL import Image
+
+
+THUMB_SIZE = (250, 250)
 
 
 class LinkFolder(models.Model):
@@ -24,7 +33,38 @@ class LinkFolder(models.Model):
         if not self.pk:
             self.created_at = timezone.now()
         self.modified_at = timezone.now()
+        if not self.make_thumbnail():
+            raise Exception('Could not create thumbnail - is the file type valid?')
         return super(LinkFolder, self).save(*args, **kwargs)
+
+    def make_thumbnail(self):
+        image = Image.open(self.image)
+        image.thumbnail(THUMB_SIZE, Image.ANTIALIAS)
+
+        thumb_name, thumb_extension = os.path.splitext(self.image.name)
+        thumb_extension = thumb_extension.lower()
+
+        thumb_filename = thumb_name + '_thumb' + thumb_extension
+
+        if thumb_extension in ['.jpg', '.jpeg']:
+            FTYPE = 'JPEG'
+        elif thumb_extension == '.gif':
+            FTYPE = 'GIF'
+        elif thumb_extension == '.png':
+            FTYPE = 'PNG'
+        else:
+            return False  # Unrecognized file type
+
+        # Save thumbnail to in-memory file as StringIO
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+
+        # set save=False, otherwise it will run in an infinite loop
+        self.image.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+
+        return True
 
     def __str__(self):
         return self.title
